30cad74f5d720173c1691599d30256a5f56c0d61
王琨 <kswangk@163.com>
2015-12-19 14:18:12 +0800

    Initial Commit

---
+++ LMSectionScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LMSectionScrollView
+//
+//  Created by Kun Wang on 15/12/19.
+//  Copyright © 2015年 luckymore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
